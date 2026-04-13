8048cb24e0021a5b91912d472ebf325891ecb846
ilousha06 <damiriilyas54@gmail.com>
2026-04-13 17:45:13 +0200

    Exo 7.19 à 7.19.2 : passage en MVC + ajout GameModel + organisation dossier images + association d’une image à chaque salle

--- zuul_v7_4/GameEngine.java
+++ zuul_v7_4/GameEngine.java
@@ -1,38 +1,38 @@
 /**
- * La classe GameEngine est le contrôleur principal du jeu Zuul en version GUI.
+ * La classe GameEngine représente le contrôleur du jeu (architecture MVC).
  * Elle gère :
- * - les déplacements du joueur
  * - l’interprétation des commandes
- * - la communication avec l’interface graphique (UserInterface)
+ * - la communication entre le modèle (GameModel) et la vue (UserInterface)
  *
- * Cette version remplace la classe Game (mode terminal).
+ * Elle ne contient pas la logique du jeu directement.
  */
 public class GameEngine
 {
-    /** Pièce dans laquelle se trouve actuellement le joueur */
-    private Room aCurrentRoom;
-
-    /** Analyseur des commandes saisies par le joueur */
+    /** Analyseur des commandes */
     private final Parser aParser;
 
-    /** Interface graphique du jeu */
+    /** Interface graphique */
     private UserInterface gui;
 
+    /** Modèle du jeu (logique) */
+    private final GameModel model;
+
     /**
-     * Construit le moteur du jeu.
-     * Initialise les pièces et le parser.
+     * Constructeur du moteur du jeu.
+     * Initialise le modèle et le parser.
      */
     public GameEngine()
     {
-        this.createRooms();
+        this.model = new GameModel();
         this.aParser = new Parser();
         this.gui = new UserInterface(this);
     }
 
     /**
-     * Définit l'interface graphique et lance le message de bienvenue.
+     * Associe l'interface graphique au moteur du jeu
+     * et affiche le message de bienvenue.
      *
-     * @param pUserInterface l'interface graphique associée au jeu
+     * @param pUserInterface l'interface graphique
      */
     public void setGUI(UserInterface pUserInterface)
     {
@@ -41,128 +41,9 @@ public class GameEngine
     }
 
     /**
-     * Crée toutes les pièces du jeu et leurs connexions.
-     * Chaque pièce est associée à une image.
-     */
-    private void createRooms()
-    {
-        Room vDortoirEast        = new Room("dans le dortoir east reserve aux soeurs", "dortoir.jpg");
-        Room vDortoirOuest       = new Room("dans le dortoir ouest plus silencieux", "dortoir2.jpg");
-        Room vRefectoire         = new Room("dans le refectoire commun", "refectoire.jpg");
-        Room vCuisine            = new Room("dans la cuisine ou les repas sont prepares", "cuisine.jpg");
-        Room vInfirmerie         = new Room("dans l infirmerie aux lumieres froides", "infirmerie.jpg");
-        Room vHallCeremonies     = new Room("dans le hall des ceremonies", "hall.jpg");
-        Room vCourIntern         = new Room("dans la cour interieure entouree de murs", "cour.jpg");
-        Room vJardinCentral      = new Room("dans le jardin central sombre", "jardin.jpg");
-        Room vPuitsAncien        = new Room("pres du puits ancien en pierre", "puits.jpg");
-        Room vSalleCachee        = new Room("dans une salle cachee sous le puits", "cachee.jpg");
-        Room vSerreCultivee      = new Room("dans la serre cultivee abandonnee", "serre.jpg");
-        Room vLabyrintheVeg      = new Room("dans le labyrinthe vegetal inquietant", "labyrinthe.jpg");
-        Room vAutelExtern        = new Room("pres de l'autel exterieur en pierre", "autel.jpg");
-        Room vCabaneJardin       = new Room("dans la cabane du jardinier", "cabane.jpg");
-        Room vChapPrinci         = new Room("dans la chapelle principale silencieuse", "chapelle.jpg");
-        Room vBiblioSacree       = new Room("dans la bibliotheque sacree", "biblio.jpg");
-        Room vArchiInterd        = new Room("dans les archives interdites", "archives.jpg");
-        Room vCloitreIntern      = new Room("dans le cloitre interieur", "cloitre.jpg");
-        Room vSalleSerments      = new Room("dans la salle des serments", "serments.jpg");
-        Room vBureauMatriarche   = new Room("dans le bureau de la matriarche", "bureau.jpg");
-        Room vAntichambreSacree  = new Room("dans l antichambre sacree", "antichambre.jpg");
-        Room vSalleRituels       = new Room("dans la salle des rituels", "rituels.jpg");
-        Room vSanctuaireIntern   = new Room("dans le sanctuaire interieur", "sanctuaire.jpg");
-        Room vCrypteAncienne     = new Room("dans la crypte ancienne", "crypte.jpg");
-        Room vChambreReliques    = new Room("dans la chambre des reliques", "reliques.jpg");
-        Room vEscalierCave       = new Room("dans l escalier menant a la cave", "escalier.jpg");
-        Room vCavePrincipale     = new Room("dans la cave principale", "cave.jpg");
-        Room vPorteScellee       = new Room("devant la porte scellee", "porte.jpg");
-        Room vSortieExtern       = new Room("devant la sortie exterieure", "sortie.jpg");
-
-        // Connexions (identiques à ton code)
-        vCourIntern.setExit("north", vDortoirOuest);
-        vCourIntern.setExit("south", vJardinCentral);
-        vCourIntern.setExit("east",  vHallCeremonies);
-        vCourIntern.setExit("west",  vRefectoire);
-
-        vDortoirEast.setExit("west", vDortoirOuest);
-        vDortoirOuest.setExit("south", vCourIntern);
-        vDortoirOuest.setExit("west", vInfirmerie);
-        vDortoirOuest.setExit("east", vDortoirEast);
-
-        vInfirmerie.setExit("east", vDortoirOuest);
-
-        vRefectoire.setExit("east", vCourIntern);
-        vRefectoire.setExit("north", vCuisine);
-        vCuisine.setExit("west", vRefectoire);
-
-        vJardinCentral.setExit("north", vCourIntern);
-        vJardinCentral.setExit("south", vPuitsAncien);
-        vJardinCentral.setExit("east", vAutelExtern);
-        vJardinCentral.setExit("west", vLabyrintheVeg);
-
-    
-        vPuitsAncien.setExit("north", vJardinCentral);
-        vPuitsAncien.setExit("down", vSalleCachee);
-        vSalleCachee.setExit("up", vPuitsAncien);
-
-        vLabyrintheVeg.setExit("east", vJardinCentral);
-        vLabyrintheVeg.setExit("west", vCabaneJardin);
-
-        vCabaneJardin.setExit("east", vLabyrintheVeg);
-        vAutelExtern.setExit("west", vJardinCentral);
-        vAutelExtern.setExit("south", vSerreCultivee);
-        vSerreCultivee.setExit("north", vAutelExtern);
-
-        vHallCeremonies.setExit("west", vCourIntern);
-        vHallCeremonies.setExit("north", vSalleSerments);
-        vHallCeremonies.setExit("south", vChapPrinci);
-
-        vSalleSerments.setExit("south", vHallCeremonies);
-        vSalleSerments.setExit("east", vAntichambreSacree);
-
-        vAntichambreSacree.setExit("south", vSalleSerments);
-        vAntichambreSacree.setExit("north", vBureauMatriarche);
-
-        vBureauMatriarche.setExit("south", vAntichambreSacree);
-
-        vChapPrinci.setExit("north", vHallCeremonies);
-        vChapPrinci.setExit("south", vSanctuaireIntern);
-        vChapPrinci.setExit("east", vBiblioSacree);
-        vChapPrinci.setExit("west", vCloitreIntern);
-
-        vCloitreIntern.setExit("east", vChapPrinci);
-        vBiblioSacree.setExit("west", vChapPrinci);
-        vBiblioSacree.setExit("north", vArchiInterd);
-        vArchiInterd.setExit("south", vBiblioSacree);
-
-        vSanctuaireIntern.setExit("north", vChapPrinci);
-        vSanctuaireIntern.setExit("south", vCrypteAncienne);
-        vSanctuaireIntern.setExit("east", vSalleRituels);
-
-        vSalleRituels.setExit("west", vSanctuaireIntern);
-
-        vCrypteAncienne.setExit("north", vSanctuaireIntern);
-        vCrypteAncienne.setExit("east", vChambreReliques);
-
-        vChambreReliques.setExit("west", vCrypteAncienne);
-
-        vCrypteAncienne.setExit("east", vEscalierCave);
-        vEscalierCave.setExit("north", vCrypteAncienne);
-
-        vEscalierCave.setExit("down", vCavePrincipale);
-        vCavePrincipale.setExit("up", vEscalierCave);
-
-        vCavePrincipale.setExit("south", vPorteScellee);
-        vPorteScellee.setExit("north", vCavePrincipale);
-
-        vPorteScellee.setExit("south", vSortieExtern);
-        vSortieExtern.setExit("north", vPorteScellee);
-
-        this.aCurrentRoom = vDortoirEast;
-    }
-
-    /**
      * Interprète une commande saisie par l'utilisateur.
      *
-     * @param pCommandLine la commande entrée sous forme de texte
+     * @param pCommandLine la commande sous forme de texte
      */
     public void interpretCommand(String pCommandLine)
     {
@@ -186,22 +67,20 @@ public class GameEngine
     }
 
     /**
-     * Affiche le message de bienvenue.
+     * Affiche le message de bienvenue du jeu.
      */
     private void printWelcome()
     {
         gui.println("=================================");
         gui.println("        LA COMMUNAUTÉ             ");
         gui.println("=================================");
-        gui.println("Une communauté religieuse fermée.");
-        gui.println("Des secrets. Des rituels. Une seule issue.");
         gui.println("Type 'help' if you need help.");
         gui.println("");
         printLocationInfo();
     }
 
     /**
-     * Affiche l'aide et la liste des commandes.
+     * Affiche l'aide et la liste des commandes disponibles.
      */
     private void printHelp()
     {
@@ -210,19 +89,17 @@ public class GameEngine
     }
 
     /**
-     * Affiche les informations de la pièce actuelle + son image.
+     * Affiche la description de la pièce actuelle
+     * ainsi que son image associée.
      */
     private void printLocationInfo()
     {
-        gui.println(aCurrentRoom.getLongDescription());
-
-        if (aCurrentRoom.getImageName() != null) { // security if image == null
-            gui.showImage(aCurrentRoom.getImageName());
-        }
+        gui.println(model.getCurrentRoom().getLongDescription());
+        gui.showImage(model.getCurrentRoom().getImageName());
     }
 
     /**
-     * Déplace le joueur vers une autre pièce.
+     * Gère le déplacement du joueur.
      *
      * @param pCommand la commande contenant la direction
      */
@@ -233,34 +110,22 @@ public class GameEngine
             return;
         }
 
-        String vDirection = pCommand.getSecondWord();
-        Room vNextRoom = this.aCurrentRoom.getExit(vDirection);
-
-        if (vNextRoom == null) {
-            gui.println("There is no door !");
-            return;
-        }
-
-        this.aCurrentRoom = vNextRoom;
+        model.goRoom(pCommand.getSecondWord());
         printLocationInfo();
     }
 
     /**
-     * Commande look : affiche la description de la pièce.
+     * Commande look : affiche la description de la pièce actuelle.
      *
      * @param pCommand la commande look
      */
     private void look(Command pCommand)
     {
-        if (pCommand.hasSecondWord()) {
-            gui.println("I don't know how to look at something in particular yet.");
-            return;
-        }
-        gui.println(this.aCurrentRoom.getLongDescription());
+        gui.println(model.getCurrentRoom().getLongDescription());
     }
 
     /**
-     * Commande eat.
+     * Commande eat : affiche un message.
      */
     private void eat()
     {
--- zuul_v7_4/UserInterface.java
+++ zuul_v7_4/UserInterface.java
@@ -3,47 +3,71 @@ import java.awt.*;
 import java.awt.event.*;
 
 /**
- * IG du jeu Zuul
+ * UI du jeu Zuul (version graphique)
+ * 👉 gère l'affichage + interactions joueur
  */
 public class UserInterface implements ActionListener
 {
-    private GameEngine engine;
-    private JFrame frame;
-    private JTextField entryField;
-    private JTextArea log;
-    private JLabel image;
+    private GameEngine engine; // lien avec le moteur du jeu
+    private JFrame frame; // la fenetre principale
+    private JTextField entryField; // zone ou on tape les commandes
+    private JTextArea log; // zone texte (histor)
+    private JLabel image; // zone pour afficher les images
 
-    // Boutons
+    // boutons commandes
     private JButton helpButton, lookButton, quitButton;
     private JButton northButton, southButton, eastButton, westButton, upButton, downButton;
 
+    /**
+     * constructeur = on lance direct l'UI
+     */
     public UserInterface(GameEngine gameEngine)
     {
-        engine = gameEngine;
-        createGUI();
+        this.engine = gameEngine;
+        createGUI(); // crée toute la fenetre
     }
 
+    /**
+     * Crée toute l'interface graphique
+     */
     private void createGUI()
     {
         frame = new JFrame("La Communauté");
-        entryField = new JTextField(34);
-
+        frame.setLayout(new BorderLayout());
+    
+        // IMAGE
+        image = new JLabel();
+        image.setHorizontalAlignment(JLabel.CENTER);
+    
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.add(image, BorderLayout.CENTER);
+        topPanel.setPreferredSize(new Dimension(800, 450));
+    
+        frame.add(topPanel, BorderLayout.NORTH);
+    
+        // TEXTE
         log = new JTextArea();
         log.setEditable(false);
-
+        log.setRows(8); // moins haut
+    
         JScrollPane listScroller = new JScrollPane(log);
-        listScroller.setPreferredSize(new Dimension(200, 200));
+        listScroller.setPreferredSize(new Dimension(600, 200));
+    
+        frame.add(listScroller, BorderLayout.CENTER);
 
-        image = new JLabel();
+        // SAISIE
+        entryField = new JTextField();
+        frame.add(entryField, BorderLayout.SOUTH);
 
-        // PANEL POUR BOUTONS
-        JPanel buttonPanel = new JPanel(new GridLayout(3, 3));
+        // BOUTONS
+        JPanel buttonPanel = new JPanel(new GridLayout(3, 3)); // grille 3x3
 
-        // Création boutons 
+        // boutons principaux
         helpButton = new JButton("help");
         lookButton = new JButton("look");
         quitButton = new JButton("quit");
 
+        // boutons déplacements
         northButton = new JButton("north");
         southButton = new JButton("south");
         eastButton = new JButton("east");
@@ -51,7 +75,7 @@ public class UserInterface implements ActionListener
         upButton = new JButton("up");
         downButton = new JButton("down");
 
-        // Ajout boutons au panel sous forme de matrice 
+        // ajout dans la grille (ordre important)
         buttonPanel.add(northButton);
         buttonPanel.add(helpButton);
         buttonPanel.add(lookButton);
@@ -64,13 +88,10 @@ public class UserInterface implements ActionListener
         buttonPanel.add(southButton);
         buttonPanel.add(downButton);
 
-        // Ajout composants à la fenêtre
-        frame.getContentPane().add(image, BorderLayout.NORTH);
-        frame.getContentPane().add(listScroller, BorderLayout.CENTER);
-        frame.getContentPane().add(entryField, BorderLayout.SOUTH);
-        frame.getContentPane().add(buttonPanel, BorderLayout.EAST);
+        frame.add(buttonPanel, BorderLayout.EAST); // a droite
 
         // LISTENERS
+        // permet de detecter actions
         entryField.addActionListener(this);
 
         helpButton.addActionListener(this);
@@ -84,40 +105,62 @@ public class UserInterface implements ActionListener
         upButton.addActionListener(this);
         downButton.addActionListener(this);
 
-        frame.pack();
-        frame.setVisible(true);
+        // FENETRE
+        frame.setSize(900, 700); // taille par défaut
+        frame.setMinimumSize(new Dimension(700, 500)); // taille mini (evite bug UI)
+        frame.setResizable(true); // permet agrandir
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // fermer = stop prog
+        frame.setVisible(true); // affiche la fenetre
     }
 
     /**
-     * Affiche du texte dans la fen
+     * affiche du texte dans le log
      */
     public void println(String text)
     {
-        log.append(text + "\n");
+        log.append(text + "\n"); // ajoute ligne
     }
 
     /**
-     * Affiche image
+     * affiche une image (adaptée à la taille du label)
      */
     public void showImage(String imageName)
     {
-        ImageIcon icon = new ImageIcon("images/" + imageName);
-        image.setIcon(icon);
-        frame.pack();
+        String path = "images/" + imageName;
+        ImageIcon icon = new ImageIcon(path);
+    
+        int maxWidth = 1200;
+        int maxHeight = 500;
+    
+        int imgWidth = icon.getIconWidth();
+        int imgHeight = icon.getIconHeight();
+    
+        double ratio = Math.min((double)maxWidth / imgWidth, (double)maxHeight / imgHeight);
+    
+        int newWidth = (int)(imgWidth * ratio);
+        int newHeight = (int)(imgHeight * ratio);
+    
+        Image scaled = icon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+    
+        image.setIcon(new ImageIcon(scaled));
+    
+        image.revalidate();
+        image.repaint();
     }
 
     /**
-     * Gestion des événements (b + t)
+     * methode appelée à chaque action
      */
     public void actionPerformed(ActionEvent e)
     {
         Object source = e.getSource();
 
-        // COMMANDES BOUTONS
+        // boutons classiques
         if(source == helpButton) engine.interpretCommand("help");
         else if(source == lookButton) engine.interpretCommand("look");
         else if(source == quitButton) engine.interpretCommand("quit");
 
+            // déplacements
         else if(source == northButton) engine.interpretCommand("go north");
         else if(source == southButton) engine.interpretCommand("go south");
         else if(source == eastButton) engine.interpretCommand("go east");
@@ -125,16 +168,16 @@ public class UserInterface implements ActionListener
         else if(source == upButton) engine.interpretCommand("go up");
         else if(source == downButton) engine.interpretCommand("go down");
 
-            // SINON : champ texte
+            // saisie clavier
         else {
-            String input = entryField.getText();
-            entryField.setText("");
-            engine.interpretCommand(input);
+            String input = entryField.getText(); // récup texte
+            entryField.setText(""); // vide la zone
+            engine.interpretCommand(input); // envoie au moteur
         }
     }
 
     /**
-     * Act/dés le jeu
+     * act/des la saisie (fin du jeu)
      */
     public void enable(boolean on)
     {
